23f7e2f3b8aad378673413d1be5971d7049a1de2
Dennis Hawran <gdatmo@gmail.com>
2021-12-06 19:58:02 +0100

    wörk again

--- hexa_op.py
+++ hexa_op.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy import context
 
 from bpy.types import Operator
 
@@ -30,8 +31,25 @@ class OBJECT_OT_HexagonGenerator(Operator):
 
     def createHexagon(self):
         # create a hexagon
-        bpy.ops.mesh.primitive_plane_add()
+        plane = bpy.ops.mesh.primitive_plane_add()
         bpy.ops.object.modifier_add(type='NODES')
-        nodeData = bpy.data.node_groups['Geometry Nodes'].nodes
+
+        # mod = bpy.data.objects[bpy.context.active_object.name].modifiers["GeometryNodes"]
+        node_group = bpy.data.node_groups["Geometry Nodes"]
+        node_group.nodes.new(type='GeometryNodeMeshCylinder')
+        node_group.nodes.new(node_item='71')
+        # new_node = 
+        # group_in = node_group.
+        # nodeData = bpy.data.node_groups['Geometry Nodes'].nodes
+        # bpy.ops.node.select()
+        
+        # bpy.ops.node.select(wait_to_deselect_others=False, deselect_all=True)
+
+
+        # bpy.ops.node.add_search(use_transform=True, node_item='97')
+        # for key in mod.keys:
+        #     print(key)
+
+
         # nodes = bpy.context.object.modifiers["Nodes"]
 
